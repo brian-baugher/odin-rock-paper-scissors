e805bd3add42ed06405a3f41ad9b874f07cec1eb
Brian Baugher <123590980+brian-baugher@users.noreply.github.com>
2023-05-27 23:39:04 -0400

    complete project (oops 1 commit)

--- script.js
+++ script.js
@@ -1,9 +1,57 @@
 function getComputerChoice(){
     let rand = Math.random()
-    if(rand<=.33){return 'Rock'}
-    else if(rand<=.66){return 'Scissors'}
-    else{return 'Paper'}
+    if(rand<=.33){return 'rock'}
+    else if(rand<=.66){return 'scissors'}
+    else{return 'paper'}
 }
 
-let ans = getComputerChoice()
-console.log(ans)
+/**
+ * 
+ * @param {String} playerSelection 
+ * @param {String} computerSelection
+ */
+function playRound(playerSelection, computerSelection){
+    let lowerPlay = playerSelection.toLowerCase()
+    switch(lowerPlay){
+        case 'rock': {
+            switch(computerSelection){
+                case 'rock': {return 0}
+                case 'paper': {return -1}
+                case 'scissors': {return 1}
+            }
+        }
+        case 'paper': {
+            switch(computerSelection){
+                case 'rock': {return 1}
+                case 'paper': {return 0}
+                case 'scissors': {return -1}
+            }
+        }
+        case 'scissors': {
+            switch(computerSelection){
+                case 'rock': {return -1}
+                case 'paper': {return 1}
+                case 'scissors': {return 0}
+            }
+        }
+        default: return 0
+    }
+}
+
+function game(){
+    let sum = 0
+    for(i = 0; i<5; i++){
+        let userAns = prompt("Your move?")
+        let compAns = getComputerChoice()
+        let result = playRound(userAns, compAns)
+        sum += result
+    }
+    if(sum > 0){
+        console.log('You win!')
+    }
+    else{
+        console.log('You lose :(')
+    }
+}
+
+game()
